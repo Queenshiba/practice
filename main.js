9a41e4d4369b010bb3b2507cd42212588ee2609d
Queenshiba <haruka0111.2a@gmail.com>
2021-05-28 23:00:28 +0900

    add delete functions

--- main.js
+++ main.js
@@ -2,18 +2,26 @@ let todoInput = document.getElementById('todoInput')
 let todoInputAdd = document.getElementById('todoInputAdd')
 
 let containerList = document.getElementById('container-list')
-
+let arr = []
 function addTodos() {
 
+    
+    arr.push(todoInput.value)
+    console.log(arr)
     let listTextNode = document.createTextNode(todoInput.value)
     let createList = document.createElement('li')
 
     createList.appendChild(listTextNode)
-    console.log(createList)
     containerList.appendChild(createList)
 
     let createDeleteBtn = document.createElement('button')
-    createDeleteBtn.textContent = "delete"
+    createDeleteBtn.textContent = "delete" 
+    createDeleteBtn.onclick = function () {
+        arr.forEach(element => {
+            alert(element);
+        });
+        
+    };
     containerList.appendChild(createDeleteBtn)
 
     let createEditBtn = document.createElement('button')
@@ -21,4 +29,9 @@ function addTodos() {
     containerList.appendChild(createEditBtn)
 
     todoInput.value = ""
+    console.log(containerList)
+}
+
+function deleteTodos() {
+
 }
