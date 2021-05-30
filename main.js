fab4c3c97338519dd84ec9dab6b05ebb46421661
Queenshiba <haruka0111.2a@gmail.com>
2021-05-30 23:53:45 +0900

    add edit btn

--- main.js
+++ main.js
@@ -5,33 +5,39 @@ let containerList = document.getElementById('container-list')
 let arr = []
 function addTodos() {
 
-    
+
     arr.push(todoInput.value)
     console.log(arr)
+    // create elememt for todo
     let listTextNode = document.createTextNode(todoInput.value)
     let createList = document.createElement('li')
-
     createList.appendChild(listTextNode)
-    
 
-let wrapList = document.createElement('div')
-wrapList.setAttribute("id", "wrapList");
-wrapList.appendChild(createList)
-
+    // create elememt for wrap each todo and btns
+    let wrapList = document.createElement('div')
+    wrapList.setAttribute("id", "wrapList");
+    wrapList.appendChild(createList)
+    // create btn for deleting btn
     let createDeleteBtn = document.createElement('button')
-    createDeleteBtn.textContent = "delete" 
-    createDeleteBtn.addEventListener('click', function(e) {
+    createDeleteBtn.textContent = "delete"
+    createDeleteBtn.addEventListener('click', function (e) {
         containerList.removeChild(wrapList)
     });
 
 
-
+    // create btn for editing btn
     let createEditBtn = document.createElement('button')
     createEditBtn.textContent = "edit"
-    // containerList.appendChild(createEditBtn)
+    createEditBtn.addEventListener('click', function (e) {
+        let createEditInput = document.createElement('input')
+        createEditInput = todoInput.value
+        console.log(todoInput.value)
+        // alert(arr)
+    });
+
 
     todoInput.value = ""
-    console.log(containerList)
+
     wrapList.appendChild(createDeleteBtn)
     wrapList.appendChild(createEditBtn)
     containerList.appendChild(wrapList)
